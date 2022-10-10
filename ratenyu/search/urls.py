44bef565c97b000b81af07dada47345d69a6c0e2
Haofan Wang <90638309+N17270182@users.noreply.github.com>
2022-10-10 18:20:17 -0400

    update views and urls config

--- ratenyu/search/urls.py
+++ ratenyu/search/urls.py
@@ -4,4 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('<str:course_id>/course_result_by_id', views.course_result_by_id, name='course_result_by_id'),
+    path('<str:professors_name>/professors_result', views.professors_result, name='professors_result'),
+    path('<str:course_name>/course_result_by_name', views.course_result_by_name, name='course_result_by_name'),
 ]
--- ratenyu/search/views.py
+++ ratenyu/search/views.py
@@ -1,6 +1,18 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 
 def index(request):
-    return HttpResponse("This is our homepage/search page.")
+    return HttpResponse("this is home/search page")
 
+
+def course_result_by_id(request, course_id):
+    return HttpResponse("You're looking at course %s." % course_id)
+
+
+def course_result_by_name(request, course_name):
+    return HttpResponse("You're looking at course %s." % course_name)
+
+
+def professors_result(request, professors_name):
+    return HttpResponse("You're looking at professor %s." % professors_name)
