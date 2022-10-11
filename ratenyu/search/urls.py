984f24b5590bf8efaa6b6ba99b52e6cfa5c933cb
Haofan Wang <90638309+N17270182@users.noreply.github.com>
2022-10-10 22:24:22 -0400

    added dummy template, and removed course_id

--- ratenyu/search/urls.py
+++ ratenyu/search/urls.py
@@ -4,7 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('<str:course_id>/course_result_by_id', views.course_result_by_id, name='course_result_by_id'),
-    path('<str:professors_name>/professors_result', views.professors_result, name='professors_result'),
-    path('<str:course_name>/course_result_by_name', views.course_result_by_name, name='course_result_by_name'),
+    path('<str:professor_name>/professor_result', views.professor_result, name='professor_result'),
+    path('<str:course_name>/course_result', views.course_result, name='course_result'),
 ]
--- ratenyu/search/views.py
+++ ratenyu/search/views.py
@@ -3,16 +3,12 @@ from django.shortcuts import render
 
 
 def index(request):
-    return HttpResponse("this is home/search page")
+    return render(request, "search/index.html")
 
 
-def course_result_by_id(request, course_id):
-    return HttpResponse("You're looking at course %s." % course_id)
+def course_result(request, course_name):
+    return render(request, "search/courseResult.html")
 
 
-def course_result_by_name(request, course_name):
-    return HttpResponse("You're looking at course %s." % course_name)
-
-
-def professors_result(request, professors_name):
-    return HttpResponse("You're looking at professor %s." % professors_name)
+def professor_result(request, professor_name):
+    return render(request, "search/professorResult.html")
