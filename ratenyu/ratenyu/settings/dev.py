70daab0c7c632dd88aa75182ed42e3abcd671af7
Nicolas Colosso <nvc9907@nyu.edu>
2022-10-11 17:51:16 -0400

    EB Dev Environment Configuration (#92)
    * AWS config
    
    * AWS config
    
    * container commands
    
    * Separate settings files for different envs and added script
    
    * AWS config
    
    * AWS config
    
    * container commands
    
    * Separate settings files for different envs and added script
    
    * removed static folder for now
    
    * cleaning up
    
    * reorganize settings files
    
    * AWS config
    
    * AWS config
    
    * container commands
    
    * Separate settings files for different envs and added script
    
    * removed static folder for now
    
    * AWS config
    
    * AWS config
    
    * container commands
    
    * Separate settings files for different envs and added script
    
    * cleaning up
    
    * reorganize settings files
    
    * AWS config
    
    * container commands
    
    * Separate settings files for different envs and added script
    
    * removed static folder for now
    
    * AWS config
    
    * AWS config
    
    * container commands
    
    * Separate settings files for different envs and added script
    
    * cleaning up
    
    * reorganize settings files
    
    * more changes
    
    * moved static dir
    
    * updated settings and styles
    
    * removing scripts folder

---
+++ ratenyu/ratenyu/settings/dev.py
@@ -0,0 +1,16 @@
+from .base import *
+
+ALLOWED_HOSTS = ['ratenyu-dev.eba-apngxcqy.us-east-1.elasticbeanstalk.com']
+DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql_psycopg2',
+            'NAME': os.environ['RDS_DB_NAME'],
+            'USER': os.environ['RDS_USERNAME'],
+            'PASSWORD': os.environ['RDS_PASSWORD'],
+            'HOST': os.environ['RDS_HOSTNAME'],
+            'PORT': os.environ['RDS_PORT'],
+        }
+}
+STATICFILES_DIRS = []
+STATIC_ROOT = 'static'
+
