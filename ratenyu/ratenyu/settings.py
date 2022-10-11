8a4e79c54fb19e5647cef7f5143b7198358b33f7
Haofan Wang <90638309+N17270182@users.noreply.github.com>
2022-10-11 11:17:25 -0400

    made a minor change in installed apps

--- ratenyu/ratenyu/settings.py
+++ ratenyu/ratenyu/settings.py
@@ -32,7 +32,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'search.apps.SearchConfig',
+    'search',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
