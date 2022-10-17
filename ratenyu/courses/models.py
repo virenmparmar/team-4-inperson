1a9c5e1289f27dc074a76dfedf347da29ee0611b
Ujjwal Kulkarni <uk2011@nyu.edu>
2022-10-17 11:36:30 -0400

    update models

--- ratenyu/courses/models.py
+++ ratenyu/courses/models.py
@@ -4,22 +4,27 @@ from ratenyu.professors.models import Professor
 
 # Create your models here.
 class Courses(models.Model):
+    courseId = models.CharField(max_length=250, primary_key=True)
     courseTitle = models.CharField(max_length=250)
+    courseSubjectCode = models.CharField(max_length=20, blank = True)
+    courseNumber = models.CharField(max_length=20, blank = True)
     courseDescription = models.TextField(blank = True)
+
 
     def __str__(self):
         return self
 class Class(models.Model):
+    classId = models.CharField(max_length=250, primary_key=True)
     professor = models.ForeignKey(Professor, on_delete=models.CASCADE)
     course = models.ForeignKey(Courses, on_delete=models.CASCADE)
     classType = models.CharField(max_length=250, blank = True)
-    classNumber = models.PositiveIntegerField(blank = True)
     classSection = models.CharField(max_length=250, blank = True)
     term = models.CharField(max_length=250, blank = True)
     lastOffered = models.CharField(max_length=250, blank = True)
     location = models.CharField(max_length=250, blank = True)
     classDescription = models.TextField(blank = True)
     capacity = models.PositiveIntegerField(blank = True)
+
     
     def __str__(self):
         return self
--- ratenyu/professors/models.py
+++ ratenyu/professors/models.py
@@ -6,16 +6,18 @@ from ratenyu.courses.models import Class
 
 # Create your models here.
 class Professor(models.Model):
+    professorId = models.CharField(max_length=250, primary_key=True)
     name = models.CharField(max_length=250)
     netId = models.CharField(max_length=250)
     role = models.CharField(max_length=250)
+
     
     def __str__(self):
         return self
 class Review(models.Model):
     reviewText = models.TextField()
     rating = models.PositiveIntegerField(choices=[1, 2, 3, 4, 5])
-    classKey = models.ForeignKey(Class, on_delete=models.CASCADE)
+    classId = models.ForeignKey(Class, on_delete=models.CASCADE)
     user = models.CharField(max_length=250)
     pubDate = models.DateTimeField('date published')
     
